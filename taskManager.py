46e4846178bafbfa472ff4fcd69387ca1544a100
Deizuu <ianis.tocaci@proton.me>
2025-08-08 01:44:07 +0300

    Added rename feature, removed 'ListTasks' function

--- taskManager.py
+++ taskManager.py
@@ -8,18 +8,31 @@ running = True
 allTasks = []
 
 while(running):
-
-    print("1: Add a task \n2: Show task list \n3: Mark a task as completed \n4: Exit the program")
+    print("1: Add a task \n2: Show task list \n3: Complete/uncomplete a task \n4: Rename a task \n5: Exit the program")
     # Registers the user's input
-    user_input = str(input("Press 1, 2, 3 or 4: "))
+    user_input = str(input("Press 1, 2, 3, 4 or 5: "))
+
     match user_input:
         case '1':
             tmf.addTask(input("Enter the name of the task: \n"), allTasks)
+            print("\n")
+
         case '2':
-            tmf.listTasks(allTasks)
-            print(allTasks)
+            print("\nHere are your tasks:\n")
+            for task in allTasks:
+                print(f"{task[0]}. {task[1]} | {"Completed" if task[2] else "Uncompleted"} \n")
+            #tmf.listTasks(allTasks)
+            #print(allTasks)
+            
         case '3':
             tmf.listTasks(allTasks)
-            tmf.completeTask(allTasks, int(input("What task do you want to mark as completed ? (enter task number): ")))
+            tmf.toggleTaskCompletion(allTasks, int(input("What task do you want to mark as completed ? (enter task number): ")))
+            print("\n")
+
         case '4':
+            tmf.listTasks(allTasks)
+            tmf.renameTask(allTasks, int(input("Which task do you want to rename ? (enter task number): ")), input("Enter new name for task: "))
+            print("\n")
+
+        case '5':
             running = False
--- taskManagerFunctions.py
+++ taskManagerFunctions.py
@@ -1,20 +1,29 @@
-import os
-def addTask(taskName:str, list:list):
-    '''The three values are:
-    1st value: task number
-    2nd value: task name
-    3rd value: task completion (False = uncompleted ; True = completed)'''
-    list.append([len(list)+1, taskName, False])
-def completeTask(taskList:list, taskNumberSearch:int):
-    for element in list:
-        if element[0] == taskNumberSearch:
-            element[2] = True
+def addTask(taskName:str, taskList:list):
+    '''meaning of the 'list.append()' below:
+    len(list)+1: refers to the task number (identifier for each task, automatically set relative to the "taskList" list parameter)
+    taskName: refers to the name of the task, usually set by the user
+    False: refers to the task completion (if the task is completed or not)
+    '''
+    taskTemplate = [len(taskList)+1, taskName, False]
+    taskList.append(taskTemplate)
+
+def toggleTaskCompletion(taskList:list, taskNumber:int):
+    for task in taskList:
+        # Changes task completion value to 'True' if the value was previously set to 'False'
+        if task[0] == taskNumber and task[2] == False:
+            task[2] = True
+
+        # Changes task completion value to 'True' if the value was previously set to 'False'
+        elif task[0] == taskNumber and task[2] == True:
+            task[2] = False
+
+def renameTask(taskList:list, taskNumber:int, newTaskName:str):
+    for task in taskList:
+        if task[0] == taskNumber:
+            task[1] = newTaskName
+
 def boolToReadable(boolean:bool):
     if boolean:
         return "Completed"
     else:
         return "Not completed"
-def listTasks(taskList:list):
-    print("Here are your tasks:")
-    for task in taskList:
-        print(f"{task[0]}. {task[1]} | {boolToReadable(task[2])} \n \n")

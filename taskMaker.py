c27d7cc0c87d68da297a9827fdcd8ea532c9fe56
Deizuu <ianis.tocaci@proton.me>
2025-08-08 11:22:47 +0300

    Changed program name and minor tweaks

--- taskMaker.py
+++ taskMaker.py
@@ -7,6 +7,12 @@ running = True
 # The list that will contain all tasks
 allTasks = []
 
+def listTasks():
+    print("\nHere are your tasks:\n")
+    for task in allTasks:
+        print(f"{task[0]}. {task[1]} | {"Completed" if task[2] else "Uncompleted"} \n")
+    
+
 while(running):
     print("1: Add a task \n2: Show task list \n3: Complete/uncomplete a task \n4: Rename a task \n5: Exit the program")
     # Registers the user's input
@@ -14,25 +20,25 @@ while(running):
 
     match user_input:
         case '1':
-            tmf.addTask(input("Enter the name of the task: \n"), allTasks)
+            print("Task created successfully." if tmf.addTask(input("Enter the name of the task: \n"), allTasks) == 0 else "Name taken. Try another name.")
             print("\n")
 
         case '2':
-            print("\nHere are your tasks:\n")
-            for task in allTasks:
-                print(f"{task[0]}. {task[1]} | {"Completed" if task[2] else "Uncompleted"} \n")
-            #tmf.listTasks(allTasks)
-            #print(allTasks)
-            
+            listTasks()
+
         case '3':
-            tmf.listTasks(allTasks)
-            tmf.toggleTaskCompletion(allTasks, int(input("What task do you want to mark as completed ? (enter task number): ")))
+            listTasks()
+            tmf.toggleTaskCompletion(allTasks, int(input("What task do you want to complete/uncomplete? (enter task number): ")))
             print("\n")
 
         case '4':
-            tmf.listTasks(allTasks)
+            #TODO: fix error when entering wrong input type
+            listTasks()
             tmf.renameTask(allTasks, int(input("Which task do you want to rename ? (enter task number): ")), input("Enter new name for task: "))
             print("\n")
 
         case '5':
             running = False
+        
+        case _:
+            print("Invalid input")
--- taskMakerFunctions.py
+++ taskMakerFunctions.py
@@ -1,11 +1,22 @@
-def addTask(taskName:str, taskList:list):
-    '''meaning of the 'list.append()' below:
-    len(list)+1: refers to the task number (identifier for each task, automatically set relative to the "taskList" list parameter)
-    taskName: refers to the name of the task, usually set by the user
+def addTask(taskName:str, taskList:list) -> int:
+    '''Template explanation:
+    len(list)+1: refers to the task number (identifier for each task, automatically assigned relative to the "taskList" list length)
+    taskName: refers to the name of the task
     False: refers to the task completion (if the task is completed or not)
     '''
     taskTemplate = [len(taskList)+1, taskName, False]
-    taskList.append(taskTemplate)
+    # Boolean value for checking if a task with the name taskName does not already exist
+    newName = True
+    for task in taskList:
+        if task[1] == taskName:
+            newName = False
+    if newName:
+        taskList.append(taskTemplate)
+    else:
+        # 1: Task not created, because name is taken
+        return 1
+    # 0: Task created successfully
+    return 0
 
 def toggleTaskCompletion(taskList:list, taskNumber:int):
     for task in taskList:
@@ -21,9 +32,3 @@ def renameTask(taskList:list, taskNumber:int, newTaskName:str):
     for task in taskList:
         if task[0] == taskNumber:
             task[1] = newTaskName
-
-def boolToReadable(boolean:bool):
-    if boolean:
-        return "Completed"
-    else:
-        return "Not completed"
